c33667f47bd34b0bf65ec1a136b38cd183ee6779
Erlyn Ascarate <erlyndavid@outlook.es>
2022-11-21 19:40:10 -0500

    Hace a la app una PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,59 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            injectRegister: 'inline',
+            includeAssets: [
+                'favicon.ico',
+                'favicon-32x32.png',
+                'favicon-16x16.png',
+                'apple-touch-icon.png',
+            ],
+            manifest: {
+                name: 'SPLITTER: Tip Calculator',
+                short_name: 'SPLITTER',
+                description:
+                    'A tip calculator\n\nAhorros iconos creados por Freepik - Flaticon\nhttps://www.flaticon.es/iconos-gratis/ahorros',
+                theme_color: '#c5e4e7',
+                background_color: '#c5e4e7',
+                orientation: 'portrait',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'maskable_icon_x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                    {
+                        src: 'maskable_icon_x1024.png',
+                        sizes: '1024x1024',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                ],
+                screenshots: [
+                    {
+                        src: 'screenshot.png',
+                        sizes: '850x1860',
+                        type: 'image/png',
+                    },
+                ],
+            },
+        }),
+    ],
 })
